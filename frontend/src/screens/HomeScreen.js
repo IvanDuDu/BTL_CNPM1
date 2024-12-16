20439e9fbc046a01043b557d8f0453fedb32ebab
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2024-12-16 21:54:03 +0700

    them slider

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row, Button, Dropdown } from "react-bootstrap";
-import Product from "../components/Product";
+import { Carousel, Col, Dropdown, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import Paginate from "../components/Paginate";
 import Meta from '../components/Meta';
+import Paginate from "../components/Paginate";
+import Product from "../components/Product";
 
 function HomeScreen({ match }) {
   const keyword = match.params.keyword || "";
@@ -33,6 +33,50 @@ function HomeScreen({ match }) {
   return (
     <div>
       <Meta />
+      
+      {/* Slider Section */}
+      <Carousel className="mb-4">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/slider/slider1.jpg"
+            alt="First slide"
+            style={{ maxHeight: "570px", objectFit: "cover" }}
+          />
+          <Carousel.Caption>
+            <h3>Welcome to Our Store</h3>
+            <p>Discover the best products at unbeatable prices!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/slider/slider2.jpg"
+            alt="Second slide"
+            style={{ maxHeight: "570px", objectFit: "cover" }}
+          />
+          <Carousel.Caption>
+            <h3>Latest Deals</h3>
+            <p>Don't miss out on our exclusive offers and discounts.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/slider/slider3.jpg"
+            alt="Third slide"
+            style={{ maxHeight: "570px", objectFit: "cover" }}
+          />
+          <Carousel.Caption>
+            <h3>Quality Products</h3>
+            <p>Shop confidently with our top-rated products.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+  <h1 className="d-flex justify-content-center align-items-center" >OUR PRODUCTS</h1>
+
+      {/* Filter Section */}
       <Row className="mb-4">
         <Col md={4}>
           <Dropdown>
@@ -42,19 +86,30 @@ function HomeScreen({ match }) {
 
             <Dropdown.Menu>
               <Dropdown.Header>By Price</Dropdown.Header>
-              <Dropdown.Item onClick={() => handlePriceFilter("lt50")}>Below $50</Dropdown.Item>
-              <Dropdown.Item onClick={() => handlePriceFilter("50to100")}>$50 - $100</Dropdown.Item>
-              <Dropdown.Item onClick={() => handlePriceFilter("gt100")}>Above $100</Dropdown.Item>
+              <Dropdown.Item onClick={() => handlePriceFilter("lt50")}>
+                Below $50
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => handlePriceFilter("50to100")}>
+                $50 - $100
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => handlePriceFilter("gt100")}>
+                Above $100
+              </Dropdown.Item>
 
               <Dropdown.Divider />
               <Dropdown.Header>By Rating</Dropdown.Header>
-              <Dropdown.Item onClick={() => handleRatingFilter("high")}>Highest Rated</Dropdown.Item>
-              <Dropdown.Item onClick={() => handleRatingFilter("low")}>Lowest Rated</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleRatingFilter("high")}>
+                Highest Rated
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => handleRatingFilter("low")}>
+                Lowest Rated
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         </Col>
       </Row>
 
+      {/* Product List Section */}
       {loading ? (
         <Loader />
       ) : error ? (
