c8e4abfc28e8774dd83e257f3a1833e4c13ded27
IvanDuDu <Dvu3999@gmail.com>
2024-12-16 15:44:21 +0700

    a

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Route } from "react-router-dom";
@@ -7,6 +7,8 @@ import "./Header.css";
 import SearchBox from "./SearchBox";
 
 const Header = ({ history }) => {
+  const [navbarTransparent, setNavbarTransparent] = useState(true);
+
   const dispatch = useDispatch();
 
   const userLogin = useSelector((state) => state.userLogin);
@@ -16,77 +18,78 @@ const Header = ({ history }) => {
     dispatch(logout());
     history.push("/login");
   };
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setNavbarTransparent(false);
+    } else {
+      setNavbarTransparent(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header>
-      <Navbar style={{backgroundColor: '#2e3192'}} data-bs-theme="light" expand="lg" collapseOnSelect>
-        <Container fluid class="d-flex">
-          <Link class="text-decoration-none " to="/">
+    <header className={`header ${navbarTransparent ? "transparent" : "solid"}`}>
+      <Navbar
+        className="fixed-top"
+        expand="lg"
+        collapseOnSelect
+        style={{ backgroundColor: navbarTransparent ? "transparent" : "#2e3192" }}
+      >
+        <Container fluid className="d-flex">
+          <Link className="text-decoration-none" to="/">
             <Navbar.Brand className="text-light fs-1 mx-4 fw-bolder">
               PhuongFourth
             </Navbar.Brand>
           </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" style={{color: "transparent",border: "1px solid black"}}/>
-          <Navbar.Collapse id="basic-navbar-nav" style={{ justifyContent: "flex-end" }}>
-
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ color: "transparent", border: "1px solid black" }}
+          />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            style={{ justifyContent: "flex-end" }}
+          >
             <Route render={({ history }) => <SearchBox history={history} />} />
-            <Nav
-              className="ml-auto my-2 my-lg-0"
-              style={{ maxHeight: "80px" }}
-              navbarScroll
-            >
-              <Link class="text-decoration-none" to= {userInfo ? "/cart" : "/login"}>
+            <Nav className="ml-auto my-2 my-lg-0" style={{ maxHeight: "80px" }} navbarScroll>
+              <Link className="text-decoration-none" to={userInfo ? "/cart" : "/login"}>
                 <Nav.Link href="/cart" className="text-light">
-                  <i
-                    className="fas fa-shopping-cart"
-                    style={{ margin: "2px", color: "#FFF" }}
-                  ></i>{" "}
-                  Cart
+                  <i className="fas fa-shopping-cart" style={{ margin: "2px", color: "#FFF" }}></i> Cart
                 </Nav.Link>
               </Link>
 
               {userInfo ? (
-                
-                <NavDropdown title={userInfo.name} id="username" >
-                  <NavDropdown.Item href="/profile" >Profile</NavDropdown.Item>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
+                <NavDropdown title={userInfo.name} id="username">
+                  <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <Link class="text-decoration-none " to="/login">
-                  <Nav.Link href="/login" style={{color:"#FFF"}}>
-                    <i
-                      className="fas fa-user"
-                      style={{ margin: "2px", color: "#FFF" }}
-                    ></i>{" "}
-                    Sign In
+                <Link className="text-decoration-none" to="/login">
+                  <Nav.Link href="/login" style={{ color: "#FFF" }}>
+                    <i className="fas fa-user" style={{ margin: "2px", color: "#FFF" }}></i> Sign In
                   </Nav.Link>
                 </Link>
               )}
 
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title="Admin" id="adminmenu">
-                  <Link class="text-decoration-none" to="/admin/userlist">
-                    <NavDropdown.Item class="ml-3" href="/admin/userList">
-                      Users
-                    </NavDropdown.Item>
+                  <Link className="text-decoration-none" to="/admin/userlist">
+                    <NavDropdown.Item href="/admin/userlist">Users</NavDropdown.Item>
                   </Link>
-                  <Link class="text-decoration-none" to="/admin/productlist">
-                    <NavDropdown.Item href="/admin/productlist">
-                      Products
-                    </NavDropdown.Item>
+                  <Link className="text-decoration-none" to="/admin/productlist">
+                    <NavDropdown.Item href="/admin/productlist">Products</NavDropdown.Item>
                   </Link>
-                  <Link class="text-decoration-none" to="/admin/orderlist">
-                    <NavDropdown.Item href="/admin/productlist">
-                      Orders
-                    </NavDropdown.Item>
+                  <Link className="text-decoration-none" to="/admin/orderlist">
+                    <NavDropdown.Item href="/admin/orderlist">Orders</NavDropdown.Item>
                   </Link>
                 </NavDropdown>
               )}
             </Nav>
           </Navbar.Collapse>
-
-          {/* </Container> */}
         </Container>
       </Navbar>
     </header>
